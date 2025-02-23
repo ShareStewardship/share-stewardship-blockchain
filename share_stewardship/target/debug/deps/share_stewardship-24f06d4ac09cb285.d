7599195d5cd9acb441e3ea7cc9dcd96d02bfe4c4
SSDAGCoreBot <DAGCoreBot@sharestewardship.org>
2025-02-23 14:15:21 -0800

    Apply cargo fmt formatting

---
+++ share_stewardship/target/debug/deps/share_stewardship-24f06d4ac09cb285.d
@@ -0,0 +1,9 @@
+/Users/gregturnquist/share-stewardship-blockchain/share_stewardship/target/debug/deps/share_stewardship-24f06d4ac09cb285: src/main.rs src/dag.rs src/validator.rs src/consensus.rs src/transaction.rs
+
+/Users/gregturnquist/share-stewardship-blockchain/share_stewardship/target/debug/deps/share_stewardship-24f06d4ac09cb285.d: src/main.rs src/dag.rs src/validator.rs src/consensus.rs src/transaction.rs
+
+src/main.rs:
+src/dag.rs:
+src/validator.rs:
+src/consensus.rs:
+src/transaction.rs:
