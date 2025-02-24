28d0223960c7407b1e312400aeac33920bb51e84
SSDAGCoreBot <DAGCoreBot@sharestewardship.org>
2025-02-24 13:23:53 -0800

    Add .gitignore to exclude build artifacts

---
+++ share_stewardship/target/debug/deps/correct_dag_module_name-7106cd68875e9ee8.d
@@ -0,0 +1,6 @@
+/Users/gregturnquist/share-stewardship-blockchain/share_stewardship/target/debug/deps/correct_dag_module_name-7106cd68875e9ee8: src/lib.rs src/dag_module.rs
+
+/Users/gregturnquist/share-stewardship-blockchain/share_stewardship/target/debug/deps/correct_dag_module_name-7106cd68875e9ee8.d: src/lib.rs src/dag_module.rs
+
+src/lib.rs:
+src/dag_module.rs:
---
+++ share_stewardship/target/debug/deps/correct_dag_module_name-ff780be206d4bf16.d
@@ -0,0 +1,9 @@
+/Users/gregturnquist/share-stewardship-blockchain/share_stewardship/target/debug/deps/correct_dag_module_name-ff780be206d4bf16: src/main.rs src/consensus.rs src/dag.rs src/transaction.rs src/validator.rs
+
+/Users/gregturnquist/share-stewardship-blockchain/share_stewardship/target/debug/deps/correct_dag_module_name-ff780be206d4bf16.d: src/main.rs src/consensus.rs src/dag.rs src/transaction.rs src/validator.rs
+
+src/main.rs:
+src/consensus.rs:
+src/dag.rs:
+src/transaction.rs:
+src/validator.rs:
